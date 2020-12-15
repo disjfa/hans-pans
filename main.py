6f435000625788dae8048d39525fff7a6cc645da
disjfa <disjfa@disjfa.nl>
2020-12-15 21:37:02 +0000

    Added dad jokes

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import discord
 import os
+import requests
 
 client = discord.Client()
+
+def get_dad_joke():
+  response = requests.get("https://icanhazdadjoke.com/", headers={'Accept': 'text/plain'})
+  return response.text
 
 @client.event
 async def on_ready():
@@ -9,11 +14,16 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
+  if message.author == client.user:
+    return
 
-    if message.content.startswith('$hello'):
-        print(message)
-        await message.channel.send('Hello!')
+  if message.content.startswith('$haha'):
+    joke = get_dad_joke();
+    await message.channel.send(joke)
+    return;
+
+  if message.content.startswith('$hello'):
+    await message.channel.send('Hello!')
+    return;
 
 client.run(os.getenv('APP_TOKEN'))
